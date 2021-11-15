ad059a265065e9d894458db53501f7d7ccc74caf
Audrey Zhao <a2zhao@ucsd.edu>
2021-11-15 01:03:20 -0800

    fixed most nav issues

--- source/Spatch/scripts/nav.js
+++ source/Spatch/scripts/nav.js
@@ -1,9 +1,22 @@
-const menuBtn = document.getElementById("menu-btn");
-const sidebar = document.querySelector(".sidebar");
-
 var path = window.location.pathname;
 var page = path.split("/").pop();
 
-menuBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("active");
+console.log(page);
+
+fetch('nav.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("#nav-placeholder");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+
+    const menuBtn = document.getElementById("menu-btn");
+    const sidebar = document.querySelector(".sidebar");
+    const otherContent = document.querySelector(".main-content");
+    menuBtn.addEventListener("click", ()=>{
+        console.log(sidebar);
+        sidebar.classList.toggle("active");
+        otherContent.classList.toggle("active");
+    });
 });
