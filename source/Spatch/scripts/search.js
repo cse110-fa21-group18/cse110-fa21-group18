8810fe57d89901893914612cf8caf6626ee076b9
Melissa Ignacio <melissaignacio@Melissas-MBP.attlocal.net>
2021-11-15 14:17:14 -0800

    Search Bar and working toggle for displaying filter options

---
+++ source/Spatch/scripts/search.js
@@ -0,0 +1,9 @@
+/*
+Function that toggles between showing filter options
+*/
+function toggleFilter(){
+  var filterPopUp = document.getElementById('showpop');
+  filterPopUp.classList.toggle("show");
+}
+
+
