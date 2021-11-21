fa23b85d5cd2b5e87feb71d047e91b1f27c27674
Melissa Ignacio <melissaignacio@Melissas-MBP.attlocal.net>
2021-11-20 21:55:35 -0800

    added event listener for search input

--- source/Spatch/scripts/search.js
+++ source/Spatch/scripts/search.js
@@ -2,8 +2,19 @@
 Function that toggles between showing filter options
 */
 function toggleFilter(){
-  var filterPopUp = document.getElementById('showpop');
-  filterPopUp.classList.toggle("show");
+  let filterPopUp = document.getElementById('showpop');
+  filterPopUp.classList.toggle('show');
 }
-
+ 
+/*
+Event listener to submit request once submit is hit for search
+*/
+let searchInput = document.getElementById('searchInput')
+let filterPopUp = document.getElementById('showpop');
+searchInput.addEventListener('keypress', function (keyBoardEvent) {
+  if (keyBoardEvent.key === 'Enter') {
+    console.log(searchInput.value);
+    alert("recipes will pop up")
+  }
+});
 
