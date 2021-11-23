f5d170b2a7d9128643e8ec85b29885a2859525d5
Melissa Ignacio <melissaignacio@Melissas-MBP.attlocal.net>
2021-11-22 18:08:21 -0800

    finished adding event listeners to all buttons

--- source/Spatch/scripts/search.js
+++ source/Spatch/scripts/search.js
@@ -1,4 +1,12 @@
 /*
+Arrays that will store filters
+*/
+let allCuisineTags = [];
+let allIngredientTags = [];
+let allDietTags = [];
+
+
+/*
 Function that toggles between showing filter options
 */
 function toggleFilter(){
@@ -7,14 +15,61 @@ function toggleFilter(){
 }
  
 /*
-Event listener to submit request once submit is hit for search
+Event listener to submit request once 'Enter' key is hit for search
 */
-let searchInput = document.getElementById('searchInput')
+let searchInput = document.getElementById('searchInput');
 let filterPopUp = document.getElementById('showpop');
 searchInput.addEventListener('keypress', function (keyBoardEvent) {
   if (keyBoardEvent.key === 'Enter') {
-    console.log(searchInput.value);
-    alert("recipes will pop up")
+    alert("recipes will pop up: " + searchInput.value + "\n" 
+      + cookingTimeFilter() + "\n" + isCheapFilters());
+    alert(allCuisineTags + "\n" + allIngredientTags + "\n" + allDietTags);
+
   }
 });
 
+
+/*
+Event listener to collect all tags for cuisine filter
+*/
+function cuisineTag(){
+  let cuisineFilter = document.getElementById('cuisine');
+  allCuisineTags.push(cuisineFilter.value);
+  cuisineFilter.value = "";
+}
+
+/*
+Event listener to collect all tags for ingredients filter
+*/
+function ingredientsTag(){
+  let ingredientFilter = document.getElementById('ingredients');
+  allIngredientTags.push(ingredientFilter.value);
+  ingredientFilter.value = "";
+}
+
+/*
+Event listener to collect all tags for diet filter
+*/
+function dietTag(){
+  let dietFilter = document.getElementById('diet');
+  allDietTags.push(dietFilter.value);
+  dietFilter.value = "";
+}
+
+
+/*
+Event listener to collect data for cooking time
+*/
+function cookingTimeFilter(){
+  let timeFilter = document.getElementById('cookTime');
+  return timeFilter.value;
+}
+
+
+/*
+Event listener to collect all for whether something is cheap
+*/
+function isCheapFilters(){
+  let cheapFilter = document.getElementById('cheap');
+  return cheapFilter.value;
+}
