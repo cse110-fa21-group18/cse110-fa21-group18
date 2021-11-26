20b1f56f13b050aa963f4aace90d1e715a4dbbae
Tianyue-Zhao <tiz001@ucsd.edu>
2021-11-26 11:06:56 -0800

    Merge and comment

--- source/Spatch/scripts/addRecipe.js
+++ source/Spatch/scripts/addRecipe.js
@@ -12,15 +12,9 @@ async function init() {
 
   //   var recipeInstructions = JSON.parse(localStorage.getItem("instructions"));
 
-  //   console.log(recipes.length);
-  //   console.log(recipeInstructions.length);
-  let ingredientCards = document.getElementById('ingredient-cards');
-  let ingredientCard = document.createElement('ingredient-card');
-  ingredientCards.appendChild(ingredientCard);
-
   let addIngredientButton = document.getElementById('add-ingredient-cards');
   addIngredientButton.addEventListener('click', createIngredientCard);
-}
+  createIngredientCard();
 
 function createIngredientCard()
 {
