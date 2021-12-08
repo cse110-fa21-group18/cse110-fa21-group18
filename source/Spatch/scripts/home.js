e245c26bef4679a4555a98e8547617e828cf4ed0
Wesley Wang <Wesley.Wang.t@gmail.com>
2021-12-07 17:12:53 -0800

    merge conflict

--- source/Spatch/scripts/home.js
+++ source/Spatch/scripts/home.js
@@ -35,17 +35,20 @@ async function init() {
 
   // ==== My Recipes ====
 
-  for (let i = 0; i < recipes.length; i++) {
-    const test = {
-      image_link: recipes[i].image,
-      reciptitle: recipes[i].title,
-      cook_time: recipes[i].readyInMinutes + " minutes",
-      tags: ["Norway food", "easy"],
-      index: i,
-    };
-    fakeCardData.push(test);
-    // console.log(recipes[i].analyzedInstructions[0].steps[0].step);
-  }
+    for (let i = 0; i < recipes.length; i++) {
+        if(recipes[i].id === -1){
+
+            const test = {
+            image_link: recipes[i].image,
+            reciptitle: recipes[i].title,
+            cook_time: recipes[i].readyInMinutes + " minutes",
+            tags: ["Norway food", "easy"],
+            index: i,
+            };
+            fakeCardData.push(test);
+        }
+        // console.log(recipes[i].analyzedInstructions[0].steps[0].step);
+    }
 
   // fetch the recipes and wait for them to load
   const main = document.querySelector("#all-recipes-cards");
