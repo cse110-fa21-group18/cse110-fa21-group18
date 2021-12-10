cfebc6a0230b37d26b1f571e5f1875c77b0f90bb
Tianyue-Zhao <tiz001@ucsd.edu>
2021-12-10 15:25:44 -0800

    Added a portion

--- source/Spatch/scripts/e2e-tests/editRecipe.test.js
+++ source/Spatch/scripts/e2e-tests/editRecipe.test.js
@@ -2,3 +2,11 @@ test('successfully edits recipe', () => {
     let editRecipe = true
     expect(editRecipe).toBe(true);
 })
+
+test('Successfully adds recipe', () => {
+    await page.goto('https://fervent-goldberg-7618a0.netlify.app/public/addrecipe');
+    await page.$eval('#recipeTitle', el => el.value = 'A sample title');
+    await page.$eval('#cuisineTypeEntry', el => el.value = 'Cuisine');
+    await page.$eval('#totalTimeEntry', el => el.value = '80');
+    expect(true).toBe(true);
+})
