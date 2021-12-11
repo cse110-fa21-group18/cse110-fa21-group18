a9b1c4d0aa20c8dfbb076feebbd9f8f2c3473b3b
Tianyue-Zhao <tiz001@ucsd.edu>
2021-12-10 22:36:29 -0800

    Changed to describe

--- source/Spatch/scripts/e2e-tests/addRecipe.test.js
+++ source/Spatch/scripts/e2e-tests/addRecipe.test.js
@@ -1,12 +1,20 @@
-test('successfully adds recipe', async () => {
+describe('Add recipe user flow', () => {
+
+beforeAll(async () => {
+    const page = await browser.newPage();
+})
+
+it('successfully adds recipe', async () => {
     let addRecipe = true
     expect(addRecipe).toBe(true);
 })
 
-test('Successfully adds recipe', async () => {
+it('Successfully adds recipe', async () => {
     await page.goto('https://fervent-goldberg-7618a0.netlify.app/public/addrecipe');
     await page.$eval('#recipeTitle', el => el.value = 'A sample title');
     await page.$eval('#cuisineTypeEntry', el => el.value = 'Cuisine');
     await page.$eval('#totalTimeEntry', el => el.value = '80');
     expect(true).toBe(true);
+})
+
 })
