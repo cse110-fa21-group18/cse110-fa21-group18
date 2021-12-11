a3d711ba2f86dcc781666bfbcbde395e821035ba
Tianyue-Zhao <tiz001@ucsd.edu>
2021-12-10 22:20:11 -0800

    Moved to addrecipe

--- source/Spatch/scripts/e2e-tests/addRecipe.test.js
+++ source/Spatch/scripts/e2e-tests/addRecipe.test.js
@@ -1,4 +1,12 @@
-test('successfully adds recipe', () => {
+test('successfully adds recipe', async () => {
     let addRecipe = true
     expect(addRecipe).toBe(true);
+})
+
+test('Successfully adds recipe', async () => {
+    await page.goto('https://fervent-goldberg-7618a0.netlify.app/public/addrecipe');
+    await page.$eval('#recipeTitle', el => el.value = 'A sample title');
+    await page.$eval('#cuisineTypeEntry', el => el.value = 'Cuisine');
+    await page.$eval('#totalTimeEntry', el => el.value = '80');
+    expect(true).toBe(true);
 })
